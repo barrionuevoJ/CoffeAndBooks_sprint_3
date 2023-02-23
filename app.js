ce33a658c861883dae4bb9166ac51fd8012dcddf
barrionuevoJ <julianbarrionuevo10@gmail.com>
2023-02-23 14:31:15 -0300

    Utilizando archivos parciales (Header), separando el css del footer y el header. Barra de busqueda agregada (no responsive)

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
 
-const mainRutas = require('./routers/main')
+const mainRutas = require('./routers/main');
+const usersRutas = require('./routers/users');
 
-app.use('/', mainRutas);
+app.use('/', mainRutas, usersRutas);
 
 app.listen(port, () => console.log(`Servidor funcionando en el puerto ${port}!`));
